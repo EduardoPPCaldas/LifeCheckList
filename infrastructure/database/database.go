fc103143076564ea73e3f9b60a38d4433eb73bc5
EduardoCaldas <eduardocaldas.dev@gmail.com>
2023-08-27 22:20:02 -0300

    feat: :sparkles: Added connection to database

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,7 +1,22 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"github.com/EduardoPPCaldas/LifeCheckList/domain/entity"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func SetDatabase() (*gorm.DB, error) {
-	
+	dsn := viper.Get("DATABASE_DNS").(string)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&entity.User{})
+
+	return db, nil
 }
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/EduardoPPCaldas/LifeCheckList/api"
+import (
+	"log"
+
+	"github.com/EduardoPPCaldas/LifeCheckList/api"
+	"github.com/EduardoPPCaldas/LifeCheckList/infrastructure/database"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
 	api := api.NewLifeCheckListApi()
+
+	_, err := database.SetDatabase()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	api.Run()
 }
